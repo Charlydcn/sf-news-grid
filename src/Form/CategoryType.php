4f00198985d82de5435cc0b703a3ceedf03a24b5
charlydcn <Charly.ducournau@outlook.fr>
2024-05-16 15:39:06 +0200

    description to CKEditor

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 
 class CategoryType extends AbstractType
 {
@@ -14,6 +15,9 @@ class CategoryType extends AbstractType
     {
         $builder
             ->add('name', TextType::class)
+            ->add('color', ColorType::class, [
+                'html5' => true,
+            ])
         ;
     }
 
